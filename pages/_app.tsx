f4bffcaedae9ef9053606d0722cf3e4220db6619
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2021-08-19 22:26:36 +0700

    remove file js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
       {/* <link rel="stylesheet" href="/assets/css/bootstrap.min.css" /> */}
       <link rel="stylesheet" href="/assets/css/font-awesome.min.css" />
-      <link rel="stylesheet" href="/assets/css/owl.carousel.min.css" />
+      {/* <link rel="stylesheet" href="/assets/css/owl.carousel.min.css" /> */}
       {/* <link rel="stylesheet" href="/assets/css/animate.min.css" /> */}
       {/* <link rel="stylesheet" href="/assets/css/jquery-ui.css" />
       <link rel="stylesheet" href="/assets/css/slick.css" />
